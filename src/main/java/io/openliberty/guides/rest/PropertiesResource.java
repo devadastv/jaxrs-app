082d3607b0c5a2d2c3f625ce627ca55d4279aa8b
devadas-tv <devadas.t.v@ibm.com>
2021-09-24 07:35:54 +0000

    Added devadas endpoint

--- src/main/java/io/openliberty/guides/rest/PropertiesResource.java
+++ src/main/java/io/openliberty/guides/rest/PropertiesResource.java
@@ -42,4 +42,13 @@ public class PropertiesResource {
 	  return new ResponseItem("/department/{id}", "dummy description"); 
 	}
 
+
+
+	@Path("/devadas") 
+	@GET 
+	@Produces(MediaType.APPLICATION_JSON) 
+	public ResponseItem get__devadas() { 
+	  return new ResponseItem("/devadas", "dummy description"); 
+	}
+
 }
